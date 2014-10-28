29008198db6357ff185d80b75cf8aebb312426e0
overlookmotel <theoverlookmotel@gmail.com>
2014-10-28 13:22:25 +0000

    Empty tests

---
+++ test/all.test.js
@@ -0,0 +1,16 @@
+// --------------------
+// shimming module
+// Tests
+// --------------------
+
+// modules
+var chai = require('chai'),
+	expect = chai.expect;
+
+// init
+chai.config.includeStack = true;
+
+// tests
+
+describe('Tests', function() {
+});
